e3cbd2b48f8acf15dc6ec954e165d648dfa22ce2
fred <fred.vatnsdal@gmail.com>
2024-07-10 17:56:34 -0400

    Remove message filtering (temporarily)

--- src/starling_interface.cpp
+++ src/starling_interface.cpp
@@ -37,33 +37,24 @@ public:
         this->get_parameter("scale", scale);
 
         // QoS
-		rmw_qos_profile_t qos_profile = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default))
-                .reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
-                .durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
-                .lifespan(rclcpp::Duration(2147483651, 294967295))
-                .deadline(rclcpp::Duration(2147483651, 294967295))
-                .liveliness(RMW_QOS_POLICY_LIVELINESS_AUTOMATIC)
-                .liveliness_lease_duration(rclcpp::Duration(2147483651, 294967295)); 
-
-        auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
+		rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
+		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
 
         // Velocity Translation (TwistStamped [GNN] to TrajectorySetpoint [PX4])
         gnn_vel_subscription_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(ns + "/cmd_vel", qos, std::bind(&StarlingInterface::publish_trajectory_setpoint, this, std::placeholders::_1));
         trajectory_setpoint_publisher_ = this->create_publisher<px4_msgs::msg::TrajectorySetpoint>(ns + "/fmu/in/trajectory_setpoint", qos);
 
         // Position Translation (VehicleLocalPosition [PX4] to PoseStamped [GNN])
-        //vehicle_local_pos_subscription_ = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
-        //        ns + "/fmu/out/vehicle_local_position", qos, std::bind(&StarlingInterface::publish_pose, this, std::placeholders::_1));
-
+        vehicle_local_position_subscription_ = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>(ns + "/fmu/out/vehicle_local_position", qos, std::bind(&StarlingInterface::publish_pose, this, std::placeholders::_1));
+        pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(ns + "/pose", qos);
+        
         // Synchronize two topics with message message
         // Required to construct PoseStamped message from local position + attitude
-        vehicle_local_pos_sub_.subscribe(this, ns + "/fmu/out/vehicle_local_position");
-        vehicle_attitude_sub_.subscribe(this, ns + "/fmu/out/vehicle_attitude");
+       // vehicle_local_pos_sub_.subscribe(this, ns + "/fmu/out/vehicle_local_position", qos->get_rmw_qos_profile());
+       // vehicle_attitude_sub_.subscribe(this, ns + "/fmu/out/vehicle_attitude", qos->get_rmw_qos_profile());
 
-        sync_.reset(new Sync(SyncPolicy(10), vehicle_local_pos_sub_, vehicle_attitude_sub_));
-        sync_->registerCallback(std::bind(&StarlingInterface::publish_pose, this, std::placeholders::_1, std::placeholders::_2));
-
-        pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(ns + "/pose", qos);
+       // sync_.reset(new Sync(SyncPolicy(10), vehicle_local_pos_sub_, vehicle_attitude_sub_));
+       // sync_->registerCallback(std::bind(&StarlingInterface::publish_pose, this, std::placeholders::_1, std::placeholders::_2));
     }
 
 private:
@@ -72,20 +63,23 @@ private:
         rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr vehicle_local_position_subscription_;
         rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
 
-        message_filters::Subscriber<px4_msgs::msg::VehicleLocalPosition> vehicle_local_pos_sub_;
-        message_filters::Subscriber<px4_msgs::msg::VehicleAttitude> vehicle_attitude_sub_;
+       // message_filters::Subscriber<px4_msgs::msg::VehicleLocalPosition> vehicle_local_pos_sub_;
+       // message_filters::Subscriber<px4_msgs::msg::VehicleAttitude> vehicle_attitude_sub_;
 
-        typedef message_filters::sync_policies::ApproximateTime<px4_msgs::msg::VehicleLocalPosition, px4_msgs::msg::VehicleAttitude> SyncPolicy;
-        typedef message_filters::Synchronizer<SyncPolicy> Sync;
-        std::shared_ptr<Sync> sync_;
-        std::atomic<uint64_t> timestamp_;
+       // typedef message_filters::sync_policies::ApproximateTime<px4_msgs::msg::VehicleLocalPosition, px4_msgs::msg::VehicleAttitude> SyncPolicy;
+       // typedef message_filters::Synchronizer<SyncPolicy> Sync;
+       // std::shared_ptr<Sync> sync_;
+       // std::atomic<uint64_t> timestamp_;
 
         void publish_trajectory_setpoint(const geometry_msgs::msg::TwistStamped::SharedPtr gnn_cmd_vel);
-        void publish_pose(const px4_msgs::msg::VehicleLocalPosition::ConstSharedPtr vehicle_local_position, const px4_msgs::msg::VehicleAttitude::ConstSharedPtr vehicle_attitude);
+        //void publish_pose(const px4_msgs::msg::VehicleLocalPosition::ConstSharedPtr vehicle_local_position, const px4_msgs::msg::VehicleAttitude::ConstSharedPtr vehicle_attitude);
+        void publish_pose(const px4_msgs::msg::VehicleLocalPosition::SharedPtr vehicle_local_position);
 };
 
 void StarlingInterface::publish_trajectory_setpoint(const geometry_msgs::msg::TwistStamped::SharedPtr gnn_cmd_vel)
 {
+    // TODO add clamp
+
     px4_msgs::msg::TrajectorySetpoint px4_msg{};
 	px4_msg.position = {std::nanf(""), std::nanf(""), std::nanf("")}; // required for vel control in px4
 	px4_msg.velocity = {gnn_cmd_vel->twist.linear.x, gnn_cmd_vel->twist.linear.y, gnn_cmd_vel->twist.linear.z};
@@ -94,17 +88,27 @@ void StarlingInterface::publish_trajectory_setpoint(const geometry_msgs::msg::Tw
 	trajectory_setpoint_publisher_->publish(px4_msg);
 }
 
-void StarlingInterface::publish_pose(const px4_msgs::msg::VehicleLocalPosition::ConstSharedPtr vehicle_local_position, const px4_msgs::msg::VehicleAttitude::ConstSharedPtr vehicle_attitude)
+//void StarlingInterface::publish_pose(const px4_msgs::msg::VehicleLocalPosition::ConstSharedPtr vehicle_local_position, const px4_msgs::msg::VehicleAttitude::ConstSharedPtr vehicle_attitude)
+//{
+//    geometry_msgs::msg::PoseStamped gnn_pose;
+//    gnn_pose.header.stamp = this->get_clock()->now();
+//    gnn_pose.pose.position.x = vehicle_local_position->x;
+//    gnn_pose.pose.position.y = vehicle_local_position->y;
+//    gnn_pose.pose.position.z = vehicle_local_position->z;
+//    gnn_pose.pose.orientation.x = vehicle_attitude->q[1];
+//    gnn_pose.pose.orientation.y = vehicle_attitude->q[2];
+//    gnn_pose.pose.orientation.z = vehicle_attitude->q[3];
+//    gnn_pose.pose.orientation.w = vehicle_attitude->q[0];
+//    pose_publisher_->publish(gnn_pose);
+//}
+
+void StarlingInterface::publish_pose(const px4_msgs::msg::VehicleLocalPosition::SharedPtr vehicle_local_position)
 {
     geometry_msgs::msg::PoseStamped gnn_pose;
     gnn_pose.header.stamp = this->get_clock()->now();
     gnn_pose.pose.position.x = vehicle_local_position->x;
     gnn_pose.pose.position.y = vehicle_local_position->y;
     gnn_pose.pose.position.z = vehicle_local_position->z;
-    gnn_pose.pose.orientation.x = vehicle_attitude->q[1];
-    gnn_pose.pose.orientation.y = vehicle_attitude->q[2];
-    gnn_pose.pose.orientation.z = vehicle_attitude->q[3];
-    gnn_pose.pose.orientation.w = vehicle_attitude->q[0];
     pose_publisher_->publish(gnn_pose);
 }
 
